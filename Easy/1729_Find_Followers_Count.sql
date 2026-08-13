4b4ae22fb0335c12e558dda476a6696735001af8
mddanishkhan-03 <md.danishkhan0307@gmail.com>
2026-08-14 04:29:57 +0530

    Add LeetCode 1729 solution

---
+++ Easy/1729_Find_Followers_Count.sql
@@ -0,0 +1,10 @@
+-- LeetCode 1729: Find Followers Count
+--
+-- Problem:
+-- Write a solution that will, for each user, return the number of followers.Return the result table ordered by user_id in ascending order.
+
+SELECT user_id,
+       COUNT(follower_id) AS followers_count
+FROM Followers
+GROUP BY user_id
+ORDER BY user_id ASC;
